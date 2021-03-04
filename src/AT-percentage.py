b30c62160d5a69f8750be8dfe8ec47b16869e42f
RodelmarOL <joserodelmar@gmail.com>
2021-03-04 13:01:47 -0600

    Se agrego un autor en reverse y se agrego el archivo AT-percentage.py

---
+++ src/AT-percentage.py
@@ -0,0 +1,26 @@
+'''NAME
+       AT-percentage
+
+VERSION
+        1.0
+
+AUTHOR
+        
+
+DESCRIPTION
+        Calculate percentage of AT on a genome sequence
+
+CATEGORY
+        Genomic sequence
+
+USAGE
+
+    % python AT-percentage.py -i filename
+    
+    example
+    
+    % python AT-percentage.py -i sequence.txt
+        
+'''
+
+
--- src/reverse-complement.py
+++ src/reverse-complement.py
@@ -6,7 +6,7 @@ VERSION
     1.0
     
 AUTHOR
-	LCG	
+	LCG CCG	
     
 DESCRIPTION
 	Make the reverse complement of DNA sequence and test for python
