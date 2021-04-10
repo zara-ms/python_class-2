b25693e848891bf476cb674a9812221b1739b9fd
RodelmarOL <joserodelmar@gmail.com>
2021-04-09 23:49:22 -0500

    Correccion de template1.py

--- templates/template1.py
+++ templates/template1.py
@@ -1,3 +1,10 @@
+
+rna = "AAGGAUGUCGCGCGUUAUUAGCCUAA"
+inicio = rna.find("AUG")
+final = rna.find("UAA")
+print("El codon de inicio empieza en posicion: ", inicio)
+print("Y la secuencia transcrita termina en :", final+3)
+print("La secuencia transcrita es :", rna[inicio:final+3])
 '''NAME
 	
 
