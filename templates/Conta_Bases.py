65948a33390c185f5e0b89d9ab3f3e43d0d72ab4
RodelmarOL <joserodelmar@gmail.com>
2021-04-21 18:33:39 -0500

    Se creo el archivo de la tarea dos de python, Conta_Bases.py

---
+++ templates/Conta_Bases.py
@@ -0,0 +1,44 @@
+'''
+
+NAME 
+	[programName].py
+
+VERSION
+	[#.#]
+
+AUTHOR
+	[Full name] <[username]@gmail.com>
+	[Other authors]: [Modifications]
+
+
+DESCRIPTION
+	[Describe the program]
+
+CATEGORY
+	[category of the program: sequence analysis for example]
+
+USAGE
+	[programName][-options/arguments]
+
+ARGUMENTS
+	[name]  [description]
+	[name]  [description]
+	[name]  [description]
+    
+INPUT
+	[files or directories used to run the program and formats]
+    
+OUTPUT
+    [file names and formats]
+    
+EXAMPLES
+    [Example 1: describe the example, input and outputs]
+	
+
+'''
+
+
+## 1. [ describe the step 1]
+
+## 2. [ describe the step 2]
+
