bc3d2b920ec2f0a0f6cd95e01af581ee8bea781f
RodelmarOL <joserodelmar@gmail.com>
2021-05-26 21:32:45 -0500

    Se subio el nuevo template2.0.py

---
+++ templates/template2.0.py
@@ -0,0 +1,48 @@
+'''
+
+NAME
+	[programName].py
+
+VERSION
+	[#.#]
+
+AUTHOR
+	[Full name] <[username]@gmail.com>
+	[Other authors]: [Modifications]
+
+
+DESCRIPTION
+	[Describe the program]
+
+CATEGORY
+	[category of the program: sequence analysis for example]
+
+USAGE
+	[programName][-options/arguments]
+
+ARGUMENTS
+	[name]  [description]
+	[name]  [description]
+	[name]  [description]
+
+INPUT
+	[files or directories used to run the program and formats]
+
+NOT WORKING INPUT
+    [specified types of files or values with which the program won't run]
+
+OUTPUT
+    [file names and formats]
+
+EXAMPLES
+    [Example 1: describe the example, input and outputs]
+
+GITHUB
+    [link to GITHUB in designated repository]
+
+'''
+
+## 1. [ describe the step 1]
+
+## 2. [ describe the step 2]
+
