f20523230d2f5cc6fb87dbada20be1fed6254d3c
RodelmarOL <joserodelmar@gmail.com>
2021-09-20 01:37:29 -0500

    archivos de la clase antes del puente de septiembre

---
+++ ejer/Clase_6.py
@@ -0,0 +1,12 @@
+from Bio import SeqIO
+from Bio.Seq import Seq
+
+path = "../src/sample.fastq"
+mala_calidad = []
+umbral = 32
+for record in SeqIO.parse(path, "fastq"):
+    promedio = sum(record.letter_annotations["phred_quality"])/len(record.letter_annotations["phred_quality"])
+    if promedio < umbral:
+        mala_calidad.append((promedio, record.id))
+
+print(len(mala_calidad))
---
+++ ejer/Clase_6_ejer2.py
@@ -0,0 +1,21 @@
+from Bio import SeqIO
+
+for gb_record in SeqIO.parse("../src/aichi.gb", "genbank"):
+    print('ID', gb_record.id)
+    print('secuencia', str(gb_record.seq)[0:30], '...')
+    print('Longitud', len(gb_record))
+
+for gb_record in SeqIO.parse("../src/virus.gb", "genbank"):
+    print('ID', gb_record.id)
+    print(gb_record.annotations)
+    version = gb_record.annotations['sequence_version']
+    organismo = gb_record.annotations['organism']
+    print(version, organimso)
+
+path = "../src/virus.gb"
+for gb_record in SeqIO.parse(path, "genbank"):
+    print('ID', gb_record.id)
+    #features
+    fuente_aislado = gb_record.features[0].qualifiers["isolation_source"]
+    pais = gb_record.features[0].qualifiers["country"]
+    print(fuente_aislado, pais)
