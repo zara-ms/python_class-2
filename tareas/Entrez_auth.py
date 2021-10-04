c78c01312378fa650831a66aeb0b540a0f13bef9
RodelmarOL <joserodelmar@gmail.com>
2021-10-04 03:07:08 -0500

    Se subieron las tareas 4 primera y segunda parte, como Entrez_auth y Entrez_prot

---
+++ tareas/Entrez_auth.py
@@ -0,0 +1,63 @@
+'''
+
+NAME
+	Entrez_auth.py
+
+VERSION
+	[1.0]
+
+AUTHOR
+	[Ocampo Luna Jose Rodelmar] <joserodelmar@gmail.com>
+	[Other authors]: [Modifications]
+
+
+DESCRIPTION
+	[Acording to input terms, a list with articles IDs will be created]
+
+CATEGORY
+	[category of the program: DataBase Analysis]
+
+USAGE
+	[Entrez_auth.py][-options/arguments]
+
+ARGUMENTS
+	[search]  [Where the input searching terms will be stored]
+	[archiv]  [Where the articles IDs corresponding to the search terms will
+	            be stored]
+
+INPUT
+	[Searching terms that match those used in pubmed search with Entrez]
+
+NOT WORKING INPUT
+    [Wrong sintaxis paths, or unexisting fields of search]
+
+OUTPUT
+    [archiv list, which stores the searching term's matching articles Ids]
+
+EXAMPLES
+    [Program is run
+    >Introduce los terminos de busqueda
+    input(Amaranta Manrique[AUTH] AND alacranes[Title] OR etica[Title])
+    print(archiv)
+    >['28350978', '23920463', '22754085', '11401128', '11008361']
+    ]
+
+GITHUB
+    [link to GITHUB in designated repository]
+
+'''
+from Bio import Entrez
+Entrez.email = "joserodelmar@gmail.com"
+print("Introduce los terminos de busqueda")
+### In "search" the users input will be stored
+search = str(input())
+### through handle the terms in "search" will be looked for in pubmed database
+handle = Entrez.esearch(db="pubmed", term=search)
+### The results that coincide with the terms in "search" are stored in result
+result = Entrez.read(handle)
+### in archiv, the list with the corresponding IDs will be stored
+archiv = result["IdList"]
+### handle must be closed
+handle.close()
+### To see the IDs that matched the search terms, archiv can be printed
+print(archiv)
---
+++ tareas/Entrez_prot.py
@@ -0,0 +1,67 @@
+'''
+
+NAME
+	Entrez_prot.py
+
+VERSION
+	[1.0]
+
+AUTHOR
+	[Ocampo Luna Jose Rodelmar] <joserodelmar@gmail.com>
+	[Other authors]: [Modifications]
+
+
+DESCRIPTION
+	[This program uses Entrez form BioPython to acces and read the
+	description of the fields ECNO (in FieldList) and
+	protein_protein_small_genome (in LinkList) from the databese
+	"protein"]
+
+CATEGORY
+	[category of the program: DataBase analysis]
+
+USAGE
+	[Entrez_prot.py][-options/arguments]
+
+ARGUMENTS
+	[field]  [used in the for cycle corresponding to the search in FieldList]
+	[linker]  [used in the for cycle corresponding to the search in LinkList]
+
+INPUT
+	[No input required, searched names already embeded]
+
+NOT WORKING INPUT
+    [Input option is non existent]
+
+OUTPUT
+    [print of the description of both fields ECNO and protein_protein_small_genome]
+
+EXAMPLES
+    [Program is run
+    >ECNO : EC number for enzyme or CAS registry number
+    protein_protein_small_genome : All proteins from this genome]
+
+GITHUB
+    [link to GITHUB in designated repository]
+
+'''
+from Bio import Entrez
+Entrez.email = "joserodelmar@gmail.com"
+### handle is used to access the data base
+handle = Entrez.einfo(db="protein")
+### record is used to "read" the elemnts accessed in handle
+record = Entrez.read(handle)
+### After use, it is important to close the access
+handle.close()
+
+### DbInfo and FieldList are accessed through the reader in record
+for field in record["DbInfo"]["FieldList"]:
+    ### When the name in FieldList matches "ECNO". the desciption of such can be printed
+    if field["Name"] == "ECNO":
+        print(field["Name"], ":", field["Description"])
+
+### Second for cycle works the same, only FieldList is replaced with LinkList and
+### the name of field searched becomes "protein_protein_small_genome"
+for linker in record["DbInfo"]["LinkList"]:
+    if linker["Name"] == "protein_protein_small_genome":
+        print(linker["Name"], ":",  linker["Description"])
