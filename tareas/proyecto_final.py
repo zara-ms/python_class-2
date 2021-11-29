ebea2cfe0f923d325a1704d2f5fc02b4ea43d691
Danigore25 <73318665+Danigore25@users.noreply.github.com>
2021-11-28 19:06:27 -0600

    Propuesta de paso de parámetros por argumentos

--- tareas/proyecto_final.py
+++ tareas/proyecto_final.py
@@ -1,11 +1,28 @@
+import argparse
 from Bio import Entrez
 Entrez.email = "joserodelmar@gmail.com"
+
 print("Introduce los terminos de busqueda")
-search = str(input())
-### through handle the terms in "search" will be looked for in pubmed database
+
+# Paso de parametros por argumentos
+parser = argparse.ArgumentParser(description="Busqueda de terminos en pubmed")
+parser.add_argument("-i", "--input",
+                    metavar="word of interest",
+                    type=str,
+                    help="Terminos a buscar",
+                    required=True)
+
+args = parser.parse_args()
+search = args.input
+
+
+# through handle the terms in "search" will be looked for in pubmed database
 handle = Entrez.esearch(db="pubmed", term=search)
-### The results that coincide with the terms in "search" are stored in result
+
+# The results that coincide with the terms in "search" are stored in result
 result = Entrez.read(handle)
-### in archiv, the list with the corresponding IDs will be stored
+
+# in archiv, the list with the corresponding IDs will be stored
 archiv = result["IdList"]
 handle.close()
+
